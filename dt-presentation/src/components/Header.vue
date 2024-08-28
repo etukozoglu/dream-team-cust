d24cf24be5f54f3fbd21f993ae6006e55f2a63ca
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2024-08-28 17:23:35 +0200

    adding navbar as component

---
+++ dt-presentation/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar bg-primary" >
+        <div class="container-fluid d-flex justify-content-center">
+            <span class="navbar h1">DreamTeam</span>
+        </div>
+    </nav> 
+</template>
+
+<script>
+    export default{
+        name: 'Header'
+    }
+</script>
