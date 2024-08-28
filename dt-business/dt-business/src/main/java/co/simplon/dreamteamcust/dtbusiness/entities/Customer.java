4ba1a36d103a1fad1584c82a891749779895446d
Diavianne <diavianne@gmail.com>
2024-08-28 22:00:26 +0200

    update entities

--- dt-business/dt-business/src/main/java/co/simplon/dreamteamcust/dtbusiness/entities/Customer.java
+++ dt-business/dt-business/src/main/java/co/simplon/dreamteamcust/dtbusiness/entities/Customer.java
@@ -1,5 +1,10 @@
 package co.simplon.dreamteamcust.dtbusiness.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Customer {
     String firstname;
     String lastname;
@@ -8,4 +13,71 @@ public class Customer {
     String message;
     String company;
     String job;
+
+    private Long id;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
+
