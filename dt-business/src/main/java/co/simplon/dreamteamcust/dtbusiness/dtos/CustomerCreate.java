26d3249d0268dd80f580d7a47b81f0ed733a5409
etukozoglu <tukozogluster@gmail.com>
2024-08-30 15:46:41 +0200

    Back-end validation

--- dt-business/src/main/java/co/simplon/dreamteamcust/dtbusiness/dtos/CustomerCreate.java
+++ dt-business/src/main/java/co/simplon/dreamteamcust/dtbusiness/dtos/CustomerCreate.java
@@ -2,13 +2,17 @@ package co.simplon.dreamteamcust.dtbusiness.dtos;
 
 import co.simplon.dreamteamcust.dtbusiness.enums.CompanySizeRange;
 import co.simplon.dreamteamcust.dtbusiness.enums.TeamSizeRange;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record CustomerCreate(
-
-	Long requestNumber, @Size(max = 100) String firstName, @Size(max = 100) String lastName, String email,
-	String phoneNumber, @Size(max = 200) String companyName, @Size(max = 50) String role,
-	@Size(max = 2000) String message, TeamSizeRange teamSize, CompanySizeRange companySize
-
-) {
+public record CustomerCreate(Long requestNumber,
+	@NotBlank(message = "{errors.firstname}") @Size(max = 100) String firstName,
+	@NotBlank(message = "{errors.lastname}") @Size(max = 100) String lastName,
+	@NotBlank(message = "{errors.email}") String email,
+	@NotBlank(message = "{errors.phone_number}") String phoneNumber,
+	@NotBlank(message = "{errors.company_name}") @Size(max = 200) String companyName, @Size(max = 50) String role,
+	@NotBlank(message = "{errors.message}") @Size(max = 2000) String message,
+	@NotNull(message = "{errors.team_size}") TeamSizeRange teamSize,
+	@NotNull(message = "{errors.company_size}") CompanySizeRange companySize) {
 }
--- dt-business/src/main/java/co/simplon/dreamteamcust/dtbusiness/dtos/CustomerView.java
+++
@@ -1,6 +0,0 @@
-package co.simplon.dreamteamcust.dtbusiness.dtos;
-
-public record CustomerView(Long request_number, String firstName, String lastName, String email, String phoneNumber,
-	String job, String message) {
-
-}
