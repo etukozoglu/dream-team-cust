26a6de2853695161254e66671611badb80ef99da
Émilie Barré <diavianne@gmail.com>
2024-09-26 16:29:01 +0200

    Create DeveloperRepository.java

---
+++ dt-business/src/main/java/co/simplon/dreamteam/customer/business/repositories/DeveloperRepository.java
@@ -0,0 +1,9 @@
+package co.simplon.dreamteamcust.dtbusiness.repositories;
+
+import co.simplon.dreamteam.customer.business.dtos.DeveloperUpload;
+import co.simplon.dreamteamcust.dtbusiness.entities.Developer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeveloperRepository extends JpaRepository<Developer, Long> {
+    DeveloperUpload save(DeveloperUpload developerUpload);
+}
