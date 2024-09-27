366e257d02d61fdc1b467d87cff0403de23ec4c7
Émilie Barré <diavianne@gmail.com>
2024-09-27 09:38:17 +0200

    Modification parse data

--- dt-business/src/main/java/co/simplon/dreamteam/customer/business/services/DeveloperService.java
+++ dt-business/src/main/java/co/simplon/dreamteam/customer/business/services/DeveloperService.java
@@ -1,31 +1,15 @@
 package co.simplon.dreamteam.customer.business.services;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
+
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.List;
 
-public void saveDevelopersFromCsv(MultipartFile file) {
-    try (CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()))) {
-
-        reader.setSeparator(';');
-        List<String[]> csvData = reader.readAll();
+@Service
 
 
-        for (String[] row : csvData) {
-            if (!row[0].equals("First Name")) { // Ignorer l'en-tête
-                String firstName = row[0];
-                String lastName = row[1];
-                String internalId = row[2];
-                String email = row[3];
 
-                System.out.println(firstName + " " + lastName + " (" + internalId + ") - " + email);
-            }
-        }
-    } catch (IOException | CsvException e) {
-        e.printStackTrace();
-    }
+public interface DeveloperService {
+    void saveDevelopersFromCsv(MultipartFile file);
 }
+
---
+++ dt-business/src/main/java/co/simplon/dreamteam/customer/business/services/DeveloperServiceImpl.java
@@ -0,0 +1,37 @@
+package co.simplon.dreamteam.customer.business.services;
+
+import co.simplon.dreamteam.customer.business.services.DeveloperService;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@Service
+public class DeveloperServiceImpl implements DeveloperService {
+
+    @Override
+    public void saveDevelopersFromCsv(MultipartFile file) {
+        try (CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream()), ';')) {
+
+            List<String[]> csvData = reader.readAll();
+
+            for (String[] row : csvData) {
+                if (!row[0].equalsIgnoreCase("First Name")) { // Ignorer l'entête
+                    String internalId = row[0];
+                    String firstName = row[1];
+                    String lastName = row[2];
+                    String email = row[3];
+
+                    System.out.println(firstName + " " + lastName + " (" + internalId + ") - " + email);
+                }
+            }
+        } catch (IOException | CsvException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
